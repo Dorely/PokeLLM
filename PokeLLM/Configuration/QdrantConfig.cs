cb06d739a7f2a95df9e746b68c7b73076a4cd231
Dorely <jont.harmon@gmail.com>
2025-07-06 11:53:27 -0700

    not working - checkpoint

---
+++ PokeLLM/Configuration/QdrantConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLLM.Game.Configuration;
+
+public class QdrantConfig
+{
+    public string Host { get; set; } = "localhost";
+    public int Port { get; set; } = 6333;
+    public string ApiKey { get; set; }
+    public bool UseHttps { get; set; } = false;
+    public string CollectionName { get; set; } = "pokemon_knowledge";
+    public int VectorSize { get; set; } = 1536; // OpenAI embeddings size
+}
